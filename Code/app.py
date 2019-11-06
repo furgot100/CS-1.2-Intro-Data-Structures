ae6f22dcd038afa15b48c2881ecac85ef0581b59
Francis Tsang <frtsang40@gmail.com>
2019-11-06 14:10:11 -0800

    heroku push

--- Code/app.py
+++ Code/app.py
@@ -4,7 +4,8 @@ from analyzer import histogram_dict
 from pymongo import MongoClient
 import os
 
-client = MongoClient()
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/tweet-generator')
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 favorite = db.favorite
 
@@ -20,4 +21,4 @@ def index():
     return render_template('index.html', tweet=sentence) 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
